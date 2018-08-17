a36806bfd0e2de39872194b6c38e2f1313618e4f
Syam Kanduri <syamkanduri@phalita.com>
2018-08-16 17:04:35 -0700

    adding initial pages

--- maps.js
+++ maps.js
@@ -1,34 +1,22 @@
 
 var address;
+var apikey = "AIzaSyATvFSKs1YEJMLy6w9qAIXKWgzoteNXrmg";
 
 $("#nearByVetsBtn").on("click", function(event){
-
     event.preventDefault();
-    var apikey = "AIzaSyATvFSKs1YEJMLy6w9qAIXKWgzoteNXrmg";
-    //var address = "19750 Auburn Dr, Cupertino, CA";
-
+    address =  $("#addressInput").val();
     var vets = "Veterinarians Near" + address;
-    console.log("vets = "+ vets);
-
     var queryURL = "https://www.google.com/maps/embed/v1/search?q=" + vets + "&key=" + apikey;
     console.log("queryURL = "+ queryURL);
-
     $("#google-map").attr("src", queryURL);
-
-
 });
 
 $("#getMapBtn").on("click", function(event){
 
     event.preventDefault();
-    var apikey = "AIzaSyATvFSKs1YEJMLy6w9qAIXKWgzoteNXrmg";
     address =  $("#addressInput").val();
-    console.log("vets = "+ address);
-
     var queryURL = "https://www.google.com/maps/embed/v1/search?q=" + address + "&key=" + apikey;
     console.log("queryURL = "+ queryURL);
-
     $("#google-map").attr("src", queryURL);
-
 
 });
